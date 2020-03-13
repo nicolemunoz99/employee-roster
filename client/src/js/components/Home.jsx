a623981bb4be027cc67c4da9bc12d8a48b5b85b5
Nicole Munoz <nicolemunoz@protonmail.com>
2020-03-13 15:08:52 -0600

    fix router stuff

--- client/src/js/components/Home.jsx
+++ client/src/js/components/Home.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { login } from '../../../urls.js';
 import EmployeeList from './EmployeeList.jsx';
-import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 
 const Home = () => {
+  const [showRedirect, updateShowRedirect] = useState(false);
+  const handleLoginClick = (e) => {
+    updateShowRedirect(true);
+  }
 
   return (
     <Router>
       <div className="container-flex home-wrapper">
         <div className="row justify-content-md-center">
           <div className="col-12 text-center display-4">
-            <Link to="/employees">
-              <span className="white-link">
-                Log in
+            {showRedirect ?
+              <Redirect to="/employees" />
+              :
+              <span className="white-link pointer" onClick={handleLoginClick}>
+              Log in
             </span>
+            }
+            
 
-            </Link>
+
           </div>
 
           <Switch>
